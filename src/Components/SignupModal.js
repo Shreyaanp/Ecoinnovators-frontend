616046be14b4e735538b51ce2b688e79b835fd31
JosephKS10 <jjbrothersdelhi@hotmail.com>
2023-12-19 16:09:01 +0530

    google auth done

--- src/Components/SignupModal.js
+++ src/Components/SignupModal.js
@@ -3,10 +3,36 @@ import './SignupModal.css'; // Make sure to create a SocialSignupModal.css file 
 import { FaFacebook } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 import { FaApple } from "react-icons/fa";
+import { auth, googleProvider } from '../firebase';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 
 const SignupModal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
+
+  const handleGoogleSignup = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+      onClose(); // Close the modal upon successful login
+    } catch (error) {
+      console.error(error);
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    }
+  };
+
 
   return (
     <div className="modal-overlay" onClick={onClose}>
@@ -17,7 +43,7 @@ const SignupModal = ({ isOpen, onClose }) => {
       </div>
       <div className='container'>
       <button className="social-button facebook"><FaFacebook />Signup with Facebook</button>
-      <button className="social-button google"><FaGoogle />Signup with Google</button>
+      <button className="social-button google" onClick={handleGoogleSignup}><FaGoogle />Signup with Google</button>
       <button className="social-button apple"><FaApple />Signup with Apple</button>
       </div>
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfvmVeoAqdLml3DaVnVTZsFEgp7dRXWEA",
+  authDomain: "sih-ecoinnovators.firebaseapp.com",
+  databaseURL: "https://sih-ecoinnovators-default-rtdb.firebaseio.com",
+  projectId: "sih-ecoinnovators",
+  storageBucket: "sih-ecoinnovators.appspot.com",
+  messagingSenderId: "1037454177507",
+  appId: "1:1037454177507:web:b8ddb2ae3eb8cbd2d5c1d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase();
+const db = getDatabase(app)
+export const googleProvider = new GoogleAuthProvider();
+
+export {db, auth, database};
